20d6f0e521e8417ce06984926a5904d1015c6c37
VRamazing <r.vignesh149@gmail.com>
2017-07-08 02:29:49 +0530

    Works for first 3

--- main.py
+++ main.py
@@ -7,6 +7,20 @@ res = requests.get("http://www.thelearningpoint.net/system/app/pages/search?scop
 res.raise_for_status()
 content =res.content #get html content
 soup = BeautifulSoup(content,'html.parser')
-element =soup.findAll("div",{"class":"sites-search-result"})
+outputFile = open('output.txt', 'w')
 
-print(element)
+
+for s in soup.findAll("div",{"class":"sites-search-result"}):
+	link=s.find("a").get('href')
+	getData = requests.get("http://www.thelearningpoint.net/" + link).content
+	# getData.raise_for_status()
+	dataSoup = BeautifulSoup(getData,'html.parser')
+	tableSoup = dataSoup.find("table",{"class":"sites-layout-name-one-column sites-layout-hbox"}).find('table')
+	print(tableSoup)
+	name = tableSoup.contents[3].find("td",{"height":"8", "width":"494"}).string.strip()
+	email = tableSoup.contents[13].find("td",{"height":"8", "width":"494"}).string.strip()
+	output = name + "," + email
+	outputFile.write(output + '\n')
+	
+	
+
